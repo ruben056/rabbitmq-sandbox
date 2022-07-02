9cf51d07c416021956526f74ed2868860bf3ae12
rdemuync <ruben.demuynck@aclvb.be>
2022-07-02 17:56:02 +0200

    "monitor" app toegevoegd: - luistert naar topic -> reactive dispatcher -> serverside event emitter. -> met google chrome connecteren en je krijgt de output te zien (firefox start download ...) ofwel client schrijven die serverside events print... Gebruikt netty ipv tomcat, lijkt meer "the way to go" voor reactive toestanden

--- monitor/src/main/java/be/drs/monitor/MonitorApplication.java
+++ monitor/src/main/java/be/drs/monitor/MonitorApplication.java
@@ -1,22 +1,33 @@
 package be.drs.monitor;
 
-import com.github.javafaker.Faker;
 import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
-import java.time.Duration;
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class MonitorApplication {
 
+	@Autowired
+	private MessageDispatcherService messageDispatcherService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+	@Bean
+	public Consumer<String> consume(){
+		return messageDispatcherService::dispatch;
 	}
 
 }
@@ -24,9 +35,29 @@ public class MonitorApplication {
 @RestController
 class SSERestController{
 
-	//todo luisteren naar topics en pushen naar geinteresseerde FE ...
+	private final MessageDispatcherService messageDispatcherService;
+
+	public SSERestController(MessageDispatcherService messageDispatcherService) {
+		this.messageDispatcherService = messageDispatcherService;
+	}
+
+
 	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE, value="/sse/{n}")
 	public Publisher<String> connectieMaken(@PathVariable String n){
-		return Flux.interval(Duration.ofSeconds(2)).map(aLong -> Faker.instance().backToTheFuture().quote());
+		return messageDispatcherService.asFlux();
+	}
+}
+
+@Service
+class MessageDispatcherService {
+
+	private final Sinks.Many<String> emitter = Sinks.many().multicast().onBackpressureBuffer();
+
+	public void dispatch(String msg) {
+		emitter.tryEmitNext(msg);
+	}
+
+	public Flux<String> asFlux() {
+		return emitter.asFlux().share();
 	}
 }
